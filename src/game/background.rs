11cd2a1b42d2fa206c0a4a02ec130a0a5d4e86db
Dan Bond <dan620@gmail.com>
2026-02-02 21:57:12 -0500

    feat: background scales with view

--- src/game/background.rs
+++ src/game/background.rs
@@ -1,11 +1,14 @@
 use bevy::prelude::*;
 use bevy::reflect::TypePath;
+use bevy::render::camera::camera_system;
 use bevy::render::render_resource::AsBindGroup;
 use bevy::shader::ShaderRef;
 use bevy::sprite_render::Material2d;
 use bevy::sprite_render::Material2dPlugin;
 
 pub fn plugin(app: &mut App) {
+    app.add_systems(PostUpdate, scale_background.after(camera_system));
+
     app.add_plugins(Material2dPlugin::<BackgroundMaterial>::default());
     app.add_observer(spawn_background);
 }
@@ -14,21 +17,39 @@ const SHADER_ASSET_PATH: &str = "shaders/background.wgsl";
 
 fn spawn_background(
     event: On<Add, Camera2d>,
+    query: Query<&Projection, Changed<Projection>>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<BackgroundMaterial>>,
 ) {
     let entity = event.event_target();
+    let ortho = match query.get(entity).unwrap() {
+        Projection::Orthographic(ortho) => ortho,
+        _ => return,
+    };
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(BackgroundMaterial::new(
             Color::srgb(0.0, 1.0, 0.0),
             Color::srgb(0.0, 0.0, 0.0),
         ))),
-        Transform::from_translation(Vec2::ZERO.extend(-100.)).with_scale(Vec3::splat(10000.)),
+        Transform::from_translation(Vec2::ZERO.extend(-100.))
+            .with_scale(ortho.area.size().extend(1.0)),
         Name::new("Background"),
         ChildOf(entity),
     ));
+}
+
+fn scale_background(
+    mut query: Query<&Projection>,
+    mut background: Query<&mut Transform, With<MeshMaterial2d<BackgroundMaterial>>>,
+) {
+    for proj in query.iter_mut() {
+        let mut transform = background.single_mut().unwrap();
+        if let Projection::Orthographic(ortho) = proj {
+            *transform = transform.with_scale(ortho.area.size().extend(1.0));
+        }
+    }
 }
 
 // This is the struct that will be passed to your shader
